0149be423dbfdd607bf37c69abaa30e6a293bcc7
Daniel <danieldaol99@gmail.com>
2022-05-03 11:22:34 -0300

    REEEEEE

--- GestaoCompromissos/ItemCompromisso.cs
+++ GestaoCompromissos/ItemCompromisso.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace GestaoCompromissos
 {
-    internal class ItemCompromisso
+    public class ItemCompromisso
     {
+        public class Compromisso
+        {
+            public string Assunto { get; set; }
+            public string Local { get; set; }
+            public string NomeContato { get; set; }
+            public DateTime HoraInicio { get; set; }
+            public DateTime HoraTermino { get; set; }
+            public DateTime DataInicio { get; set; }
+            public DateTime DataCompromisso { get; set; }
+
+            public Compromisso()
+            {
+
+            }
+            public Compromisso(string assunto, string local, string contato, DateTime horaInicio, DateTime horaTermino, DateTime dataInicio)
+            {
+                Assunto = assunto;
+                Local = local;
+                NomeContato = contato;
+                HoraInicio = horaInicio;
+                HoraTermino = horaTermino;
+                DataInicio = dataInicio;
+
+            }
+        }
     }
 }
